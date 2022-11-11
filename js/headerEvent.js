b1a6f2d976b0d2fd71cfb3e0654b1de3cf16a5b5
wyw231 <wonyongwan@gmail.com>
2022-11-11 15:58:02 +0900

    맞춤형 기장 서비스 반응형

--- js/headerEvent.js
+++ js/headerEvent.js
@@ -1,5 +1,9 @@
 // header event
+const mobileBg = document.querySelector('.mobileBg')
+const header = document.querySelector('#header')
 const headerList = document.querySelectorAll('#header > ul > li');
+const headerMobileOpenBtn = document.querySelector('#headerM > button');
+const headerMobileCloseBtn = document.querySelector('#header > button');
 
 for(let item of headerList) {
   function addActive() {
@@ -12,3 +16,14 @@ for(let item of headerList) {
   item.addEventListener('mouseover', addActive);
   item.addEventListener('mouseout', removeActive);
 }
+
+function headerAddActive() {
+  header.classList.add('active');
+  mobileBg.classList.add('active')
+}
+function headerRemoveActive() {
+  header.classList.remove('active');
+  mobileBg.classList.remove('active');
+}
+headerMobileOpenBtn.addEventListener('click', headerAddActive);
+headerMobileCloseBtn.addEventListener('click', headerRemoveActive);
